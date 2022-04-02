7b86ebcb67855f668f76961085f63a2cf41840c8
showyaa <510shoya@gmail.com>
2022-04-02 15:19:23 +0900

    first-commit

---
+++ result.php
@@ -0,0 +1,9 @@
+<?php
+$my_name = htmlspecialchars($_POST['my_name'], ENT_QUOTES);
+$choices = htmlspecialchars($_POST['choices'], ENT_QUOTES);
+$number = htmlspecialchars($_POST['number'], ENT_QUOTES);
+?>
+
+<p>私の名前は、<?php echo $my_name;?></p>
+<p>ご希望商品は、<?php echo $choices ?></p>
+<p>御注文数は、<?php echo $number ?></p>
